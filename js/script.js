36393e43fc491445ecbb0a8891903d205f37c555
brunofsilva2 <bruno.19fs@gmail.com>
2024-11-29 22:30:25 -0300

    Atualização do Portfólio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function toogleMode() {
+    const html = document.documentElement;
+    html.classList.toggle("light");
+
+    const img = document.querySelector("#profile img");
+
+    if(html.classList.contains("light")) {
+        img.setAttribute("src", "./img/avatar2.jpg");
+    } else{
+        img.setAttribute("src", ".img/avatar1.jpeg");
+    }
+}
